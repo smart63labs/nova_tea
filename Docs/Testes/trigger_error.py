f61caf04186b8782a2b394c390f8b92237999c09
Anderson Silva Dorneles <dornelesgpi@gmail.com>
2025-12-23 13:27:03 -0300

    Editado em 23-12-2025 as 13:23 hrs
    Editado em 23-12-2025 as 13:23 hrs

--- Docs/Testes/trigger_error.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:3001/api/knowledge/stores/fileSearchStores/secretaria-da-fazenda-eij7m6bp2i0b/files"
-try:
-    resp = requests.get(url)
-    print(resp.status_code)
-    print(resp.text)
-except Exception as e:
-    print(e)
--- tea/__init__.py
+++
@@ -1,3 +0,0 @@
-from .agent import root_agent
-# Expose as agent as well for backward compatibility or different loader expectations
-agent = root_agent
